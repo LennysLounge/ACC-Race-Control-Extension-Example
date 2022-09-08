24feb1b21bf1c7c90b245690840fff0198be7a94
Leonard Schüngel <schuengel.leonard@googlemail.com>
2022-09-08 22:14:55 +0200

    Init Repo.

---
+++ src/main/java/racecontrol/example/ExampleExtensionModule.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2021 Leonard Schüngel
+ * 
+ * For licensing information see the included license (LICENSE.txt)
+ */
+package racecontrol.example;
+
+import racecontrol.appextension.AppExtensionModule;
+import racecontrol.client.ClientExtension;
+import racecontrol.gui.app.PageController;
+
+/**
+ * An Example of an extension module.
+ *
+ * @author Leonard
+ */
+public class ExampleExtensionModule implements AppExtensionModule {
+
+    @Override
+    public String getName() {
+        return "Example extension";
+    }
+
+    @Override
+    public ClientExtension getExtension() {
+        return null;
+    }
+
+    @Override
+    public PageController getPageController() {
+        return null;
+    }
+
+}
